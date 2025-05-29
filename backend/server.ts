00f924d219f30ed0e34ebc200c4555464d3f3337
Austin <austincumberlander@gmail.com>
2025-05-29 15:59:02 -0500

    More updates...

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import studentRoutes from "./routes/studentRoutes.js";
+import studentRoutes from "./routes/studentRoutes";
 
 dotenv.config();
 
